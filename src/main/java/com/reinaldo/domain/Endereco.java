b00d0f0d980526082d40bf76dda5eebfd28728fa
Rein4ldo <naldos15@hotmail.com>
2022-04-16 11:30:52 -0300

    path GET clientes/id works

--- src/main/java/com/reinaldo/domain/Endereco.java
+++ src/main/java/com/reinaldo/domain/Endereco.java
@@ -32,6 +32,7 @@ public class Endereco implements Serializable {
 	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
 
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
